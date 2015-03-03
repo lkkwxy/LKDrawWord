90504666d54f18da471424222b353025b48f4480
likunkun <1403705809@qq.com>
2015-03-03 16:52:42 +0800

    上传项目

---
+++ LKDrawWord/LKDrawWord/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  LKDrawWord
+//
+//  Created by lkk on 15-2-5.
+//  Copyright (c) 2015年 lkk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var word: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+//      CFString 
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+       
+    }
+
+    @IBAction func beganDraw(sender: UIButton) {
+        for(var i = 0; i < self.view.layer.sublayers.count; i++){
+            var layer: AnyObject = self.view.layer.sublayers[i]
+            if(layer.isKindOfClass(LKDrawWordLayer)){
+                layer.removeFromSuperlayer()
+            }
+        }
+         LKDrawWordLayer.createAnimation(word.text, font: UIFont.systemFontOfSize(50), rect: CGRectMake(0, 118, self.view.frame.size.width, self.view.frame.size.height - 118), view: self.view, color: UIColor.redColor())
+        self.view.endEditing(true)
+        println(sender.frame)
+    }
+   
+
+}
+
